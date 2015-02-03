8476f37d12f7176a77ef4adb8a0baba9ce3d0a2e
actiontwo <actiontwo@designveloper.com>
2015-02-03 08:56:32 +0700

    done menu

--- html/js/script.js
+++ html/js/script.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('#main-slider .slider').bxSlider();
+
+  showMenuList();
+
 });
+
+var showMenuList = function () {
+  var mainMenu = $('.main-menu');
+  var subMenu = $('.sub-menu');
+  mainMenu.find('li').hover(function(){
+    if($(this).hasClass('active-menu')){
+      return;
+    }
+    mainMenu.find('li').removeClass('active-menu');
+    subMenu.hide();
+    var menuId = $(this).addClass('active-menu').data('menu');
+    $('#'+menuId).css({display:'table'})
+  })
+};
